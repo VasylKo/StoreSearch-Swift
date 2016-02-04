de86bdc63650abee42af3f57d1170ea5211ff4eb
Vasiliy Kotsiuba <clic@ukr.net>
2016-02-04 17:27:35 +0300

    added table view and search bar

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+  //MARK: - Outlets
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var tableView: UITableView!
+  
+  //MARK: - Ivars
+  
+  //MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+  //costumize table view insets not to overlap serach bar
+    tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0) //made up of 20 points for the status bar and 44 points for the Search Bar
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
+extension SearchViewController: UISearchBarDelegate {
+  func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+    print("The search text is: '\(searchBar.text!)'")
+  }
+}
+
+
+extension SearchViewController: UITableViewDataSource {
+  
+}
+
+extension SearchViewController: UITableViewDelegate {
+  
+}
