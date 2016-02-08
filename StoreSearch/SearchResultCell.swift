0517fe2d7982d280f44283babc11eafa8bc7b63b
Vasiliy Kotsiuba <clic@ukr.net>
2016-02-08 18:47:38 +0300

    code refactoreing

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -27,10 +27,33 @@ class SearchResultCell: UITableViewCell {
     selectedBackgroundView = selectedView
   }
 
-  override func setSelected(selected: Bool, animated: Bool) {
-      super.setSelected(selected, animated: animated)
-
-      // Configure the view for the selected state
+  func configureForSearchResult(searchResult: SearchResult) {
+    nameLabel.text = searchResult.name
+    
+    if searchResult.artistName.isEmpty {
+      artistNameLabel.text = "Unknown"
+    } else {
+      artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, kindForDisplay(searchResult.kind))
+    }
   }
+  
+  //MARK: - Helper
+  func kindForDisplay(kind: String) -> String {
+    
+    switch kind {
+    case "album": return "Album"
+    case "audiobook": return "Audio Book"
+    case "book": return "Book"
+    case "ebook": return "E-Book"
+    case "feature-movie": return "Movie"
+    case "music-video": return "Music Video"
+    case "podcast": return "Podcast"
+    case "software": return "App"
+    case "song": return "Song"
+    case "tv-episode": return "TV Episode"
+    default: return kind
+    }
+  }
+
 
 }
